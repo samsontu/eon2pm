18b24269a00e9f70db74cc246cdb1cecf04fc90b
Samson Tu <swt@stanford.edu>
2017-08-16 10:49:29 -0700

    Remove edu.stanford.smi/* (mostly class files) from version control

--- batch/GlycemicControl.sh
+++ batch/GlycemicControl.sh
@@ -1,1 +1,1 @@
-./GenericATHENA.sh ATHENA_GlycemicControl.pprj ini/DMGlycemicControlTestingEnvironment.ini
+./ATHENAGeneric.sh  /Users/tu/workspace/eon2pm /Users/tu/Documents/Dropbox/VA/ATHENA/athenakbs/ATHENA_DM_GlycemicControl_Dashboard.pprj ini/TemplateTestingEnvironment.ini
