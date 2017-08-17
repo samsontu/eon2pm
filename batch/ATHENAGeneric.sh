2343c083ebe7e8055600911964fd70374c61b023
Samson Tu <swt@stanford.edu>
2017-08-17 13:55:41 -0700

    Added lib/plugins directory and made lib consistent with Protege installation

---
+++ batch/ATHENAGeneric.sh
@@ -0,0 +1,23 @@
+
+MAXIMUM_MEMORY=-Xmx512M
+OPTIONS=$MAXIMUM_MEMORY
+EONHOME=$1
+GUIDELINEFILE=$2
+TestEnvironmentINI=$EONHOME/$3
+LIB=$EONHOME/lib
+
+JRE=$JAVA_HOME
+PCADIR=$LIB/plugins/edu.stanford.smi.eon.pca
+
+PATH=$PATH:JRE/bin
+
+
+PROTEGEJARS=$LIB/protege.jar:$LIB/looks.jar:$LIB/unicode_panel.jar
+GUIDELINEJARS=$PCADIR/eonpm.jar:$PCADIR/AthenaPerformanceMetrics.jar:$LIB/plugins/chronusII.jar:$PCADIR/eonpal.jar:$PCADIR/antlr.jar:$LIB/jcchart.jar:$LIB/jctable.jar:$PCADIR/log4j.jar
+TESTJARS=$LIB/jcalendar.jar:$LIB/gov.va.test.opioidtesttool/EONTestingEnvironment.jar:$LIB/gov.va.test.opioidtesttool/test.jar
+CLASSPATH=$PROTEGEJARS:$GUIDELINEJARS:$TESTJARS
+
+$JRE/bin/java $OPTIONS  -Dprotege.dir=$LIB   -cp $CLASSPATH -DEON_HOME=$EONHOME   -DTestEnvironmentINI=$TestEnvironmentINI edu.stanford.smi.protege.Application $GUIDELINEFILE
+
+
+
---
+++ batch/ATHENATemplate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+TestEnvironment=/Users/tu/workspace/eon2pm
+KBDir=/Users/tu/Documents/Dropbox/VA/ATHENA/athenakbs
+KB=$KBDir/ATHENA_DM_GlycemicControl_Dashboard.pprj
+
+./ATHENAGeneric.sh  $TestEnvironment $KB ini/TemplateTestingEnvironment.ini
