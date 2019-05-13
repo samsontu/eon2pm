30af31df2c088fcac50e8ef500c38ddb5487dbb0
Samson Tu <swt@stanford.edu>
2019-05-12 22:04:22 -0700

    Got BatchDMGlycemicControlDashboard to work

--- batch/BatchDMGlycemicControlDashboard.bat
+++ batch/BatchDMGlycemicControlDashboard.bat
@@ -3,4 +3,4 @@ set INIFILE=BatchDMGlycemicControlDashboard.ini
 set KBPATH=C:\appData\athenakbs\ATHENA_DM_GlycemicControl_Dashboard.pprj
 set LOGFILE=BatchDMGlyCntrlDash.log
 
-BatchGeneric.bat %EONHOME %KBPATH% "ini\%INIFILE%" > "..\testresult\%LOGFILE"
+BatchGeneric.bat %EONHOME% %KBPATH% ini\%INIFILE% > ..\testresult\%LOGFILE%
